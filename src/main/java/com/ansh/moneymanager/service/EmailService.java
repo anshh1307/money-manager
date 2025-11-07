83af733c2e5ba9ff003497121175736949f96bda
anshh1307 <anshagarwal004@gmail.com>
2025-11-07 13:37:21 +0530

    Replaced Brevo SMTP with Api Key to bypass blocked smtp ports.

--- src/main/java/com/ansh/moneymanager/service/EmailService.java
+++ src/main/java/com/ansh/moneymanager/service/EmailService.java
@@ -2,30 +2,45 @@ package com.ansh.moneymanager.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import sendinblue.ApiClient;
+import sendinblue.Configuration;
+import sibApi.TransactionalEmailsApi;
+import sibModel.SendSmtpEmail;
+import sibModel.SendSmtpEmailSender;
+import sibModel.SendSmtpEmailTo;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
-    private final JavaMailSender javaMailSender;
+
+    @Value("${BREVO_API_KEY}")
+    private String apiKey;
 
     @Value("${BREVO_FROM_EMAIL}")
     private String fromEmail;
 
     @Async
     public void sendEmail(String to, String subject, String body){
-        try{
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(fromEmail);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(body);
-            javaMailSender.send(message);
+        try {
+            ApiClient client = Configuration.getDefaultApiClient();
+            client.setApiKey(apiKey);
+
+            TransactionalEmailsApi apiInstance = new TransactionalEmailsApi();
+            SendSmtpEmailSender sender = new SendSmtpEmailSender().email(fromEmail);
+            SendSmtpEmailTo receiver = new SendSmtpEmailTo().email(to);
+
+            SendSmtpEmail email = new SendSmtpEmail()
+                    .sender(sender)
+                    .addToItem(receiver)
+                    .subject(subject)
+                    .htmlContent(body);
+
+            apiInstance.sendTransacEmail(email);
+
         } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+            throw new RuntimeException("Email sending failed: " + e.getMessage());
         }
     }
 }
